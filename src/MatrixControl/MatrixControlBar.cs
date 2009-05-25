6dd20a76fc56c3873deaad5c76a104a902c365f1
clough42@hotmail.com <clough42@hotmail.com@8cf5c726-47fe-11de-a65c-33beeba39556>
2009-05-25 17:53:03 +0000

    Initial checkin.

---
+++ src/MatrixControl/MatrixControlBar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BandObjectLib;
+using System.Runtime.InteropServices;
+
+namespace MatrixControl
+{
+    [Guid("123568A6-43E0-11DE-A2FC-CBA955D89593")]
+    [BandObject("Matrix Control Bar", BandObjectStyle.Horizontal | BandObjectStyle.ExplorerToolbar | BandObjectStyle.TaskbarToolBar, HelpText = "Shows bar that says hello.")]
+    public partial class MatrixControlBar : BandObject
+    {
+        public MatrixControlBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
