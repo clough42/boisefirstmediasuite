8118340012949e9c4f826382b0f682a3cb6f43a2
clough42@hotmail.com <clough42@hotmail.com@8cf5c726-47fe-11de-a65c-33beeba39556>
2009-06-01 17:20:25 +0000

    Added about box.

---
+++ trunk/src/MatrixControl/AboutBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace MatrixControl
+{
+    public partial class AboutBox : Form
+    {
+        public AboutBox()
+        {
+            InitializeComponent();
+
+            this.versionLabel.Text = "Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(this.urlLinkLabel.Text);
+        }
+    }
+}
