c32cc8a9dd96dcc42a5947406ec8cc8898ca57f4
clough42@hotmail.com <clough42@hotmail.com@8cf5c726-47fe-11de-a65c-33beeba39556>
2009-06-01 12:12:02 +0000

    Fixed install/uninstall scripts to automatically switch between 32- and 64-bit windows.

--- trunk/live/installmatrixcontrol.bat
+++ trunk/live/installmatrixcontrol.bat
@@ -2,5 +2,6 @@
 "%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\gacutil" /if "BandObjectLib.dll"
 "%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\gacutil" /if "Interop.ShDocVw.dll"
 "%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\gacutil" /if "MatrixControl.dll"
-rem "%SYSTEMROOT%\Microsoft.NET\Framework\v2.0.50727\regasm" "MatrixControl.dll"
-"%SYSTEMROOT%\Microsoft.NET\Framework64\v2.0.50727\regasm" "MatrixControl.dll"
+
+IF EXIST "%SYSTEMROOT%\Microsoft.NET\Framework" "%SYSTEMROOT%\Microsoft.NET\Framework\v2.0.50727\regasm" "MatrixControl.dll"
+IF EXIST "%SYSTEMROOT%\Microsoft.NET\Framework64" "%SYSTEMROOT%\Microsoft.NET\Framework64\v2.0.50727\regasm" "MatrixControl.dll"
